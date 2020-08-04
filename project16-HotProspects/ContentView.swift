0430842f271e6b8740c7fd5aa3ed48269c53b69e
BingyuanXu <xubingyuan123@hotmail.com>
2020-08-03 23:51:31 -0500

    using wiliest {objectwillchange.snend() }  instead of @published

--- project16-HotProspects/ContentView.swift
+++ project16-HotProspects/ContentView.swift
@@ -8,56 +8,28 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  enum NetworkError: Error {
-    case badURL, requestFailed, unknown
+class DelayedUpdater: ObservableObject {
+  var value = 0 {
+    willSet {
+      objectWillChange.send()
+    }
   }
   
-  func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
-    // check the URL is OK, otherwise return with a failure
-    guard let url = URL(string: urlString) else {
-      completion(.failure(.badURL))
-      return
-    }
-    
-    URLSession.shared.dataTask(with: url) { data, response, error in
-      // the task has completed – push our work back to the main thread
-      DispatchQueue.main.async {
-        if let data = data {
-          // success: convert the data to a string and send it back
-          let stringData = String(decoding: data, as: UTF8.self)
-          completion(.success(stringData))
-        } else if error != nil {
-          // any sort of network failure
-          completion(.failure(.requestFailed))
-        } else {
-          // this ought not to be possible, yet here we are
-          completion(.failure(.unknown))
-        }
+  init() {
+    for i in 1...10 {
+      DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)){
+        self.value += 1
       }
-    }.resume()
+    }
   }
+}
+
+struct ContentView: View {
+  @ObservedObject var updater = DelayedUpdater()
+  
   
   var body: some View {
-    Text("Hello, World!")
-      .onAppear {
-        self.fetchData(from: "https://www.apple.com") { result in
-          switch result {
-          case .failure(let error) :
-            switch error {
-            case .badURL:
-              print("bad URL")
-            case .requestFailed:
-              print("request Failed")
-            case .unknown:
-              print("unknown")
-            }
-            
-          case .success(let str):
-            print(str)
-          }
-        }
-    }
+    Text("value is : \(updater.value)")
   }
 }
 
