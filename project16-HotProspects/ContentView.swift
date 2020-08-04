6838581f56e2a324b82db949a555d0b6a0365c84
BingyuanXu <xubingyuan123@hotmail.com>
2020-08-03 22:46:18 -0500

    using Result data type to handle URL request

--- project16-HotProspects/ContentView.swift
+++ project16-HotProspects/ContentView.swift
@@ -9,28 +9,54 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var selectedTab = 0
+  enum NetworkError: Error {
+    case badURL, requestFailed, unknown
+  }
+  
+  func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
+    // check the URL is OK, otherwise return with a failure
+    guard let url = URL(string: urlString) else {
+      completion(.failure(.badURL))
+      return
+    }
+    
+    URLSession.shared.dataTask(with: url) { data, response, error in
+      // the task has completed – push our work back to the main thread
+      DispatchQueue.main.async {
+        if let data = data {
+          // success: convert the data to a string and send it back
+          let stringData = String(decoding: data, as: UTF8.self)
+          completion(.success(stringData))
+        } else if error != nil {
+          // any sort of network failure
+          completion(.failure(.requestFailed))
+        } else {
+          // this ought not to be possible, yet here we are
+          completion(.failure(.unknown))
+        }
+      }
+    }.resume()
+  }
+  
   var body: some View {
-    TabView(selection: $selectedTab) {
-      Text("View 1")
-        .onTapGesture {
-          self.selectedTab = 1
-      }
-      .tabItem {
-        Image(systemName: "star")
-        Text("tab 1")
-      }
-      .tag(0)
-      
-      Text("View 2")
-        .onTapGesture {
-          self.selectedTab = 0
-      }
-        .tabItem {
-          Image(systemName: "star.fill")
-          Text("tab 2")
-      }
-      .tag(1)
+    Text("Hello, World!")
+      .onAppear {
+        self.fetchData(from: "https://www.apple.com") { result in
+          switch result {
+          case .failure(let error) :
+            switch error {
+            case .badURL:
+              print("bad URL")
+            case .requestFailed:
+              print("request Failed")
+            case .unknown:
+              print("unknown")
+            }
+            
+          case .success(let str):
+            print(str)
+          }
+        }
     }
   }
 }
