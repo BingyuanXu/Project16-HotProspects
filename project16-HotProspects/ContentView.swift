f168122ee98673048cdf94e7ae352bb5cf04d1c3
BingyuanXu <xubingyuan123@hotmail.com>
2020-08-04 01:57:13 -0500

    notification

--- project16-HotProspects/ContentView.swift
+++ project16-HotProspects/ContentView.swift
@@ -9,35 +9,33 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var backgroundColor = Color.black
+  
   var body: some View {
     VStack {
-      Text("Hello World")
-        .padding()
-        .background(backgroundColor)
+      Button("Request Permission") {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+          if success {
+            print("All set!")
+          } else if let error = error {
+            print(error.localizedDescription)
+          }
+        }
+      }
       
-      Text("Change background color")
-        .padding()
-        .contextMenu {
-          Button(action: {
-              self.backgroundColor = .red
-          }) {
-              Text("Red")
-              Image(systemName: "checkmark.circle.fill")
-                  .foregroundColor(.red)
-          }
-          
-          Button(action: {
-            self.backgroundColor = .green
-          }) {
-            Text("Green")
-          }
-          
-          Button(action: {
-            self.backgroundColor = Color.blue
-          }) {
-            Text("Blue")
-          }
+      Button("Schedule Notification") {
+        let content = UNMutableNotificationContent()
+        content.title = "Feed the cat"
+        content.subtitle = "It looks hungry"
+        content.sound = UNNotificationSound.default
+        
+        // show this notification five seconds from now
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        // choose a random identifier
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        // add our notification request
+        UNUserNotificationCenter.current().add(request)
       }
     }
   }
